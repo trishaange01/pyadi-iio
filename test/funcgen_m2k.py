2bd72f2eecaa309a554db2a16b04dc1b039c3207
Julia Pineda <Julia.Pineda@analog.com>
2022-10-24 13:33:32 +0800

    M2k as siggen initial commit
    Signed-off-by: Julia Pineda <Julia.Pineda@analog.com>

--- test/funcgen_m2k.py
+++ test/funcgen_m2k.py
@@ -1,57 +1,37 @@
-import matplotlib
-import numpy as np
-import wave, math
+import subprocess
+import time
 
-from matplotlib import pyplot as plt
-# Variables
-freq = 20000 # frequency of first sine wave
-sRate = 2*freq # sample rate in Hertz
-amplitude1 = 1 # amplitude of first wave
-numPeriods = 1 # number of periods of the sine waves
-numSamples = sRate * numPeriods # total number of samples
+global scopy_path
+global m2k_config
 
-# Graphing helper function
-def setup_graph(title='', x_label='', y_label='', fig_size=None):
-    fig = plt.figure()
-    if fig_size != None:
-        fig.set_size_inches(fig_size[0], fig_size[1])
-    ax = fig.add_subplot(111)
-    ax.set_title(title)
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
+scopy_path = "C:\Program Files\Scopy\Scopy.exe"
+m2k_config = "instr_config.js"
+# m2k_config = "siggen_spec.js"
+
+def func_on(instr, func, freq, ampl, offset):
+    if instr == "m2k":
+        
+        copy_conf = subprocess.run('copy instr_config_base.js instr_config.js', shell=True)
+        instr_config = open("instr_config.js","a")
+        instr_config.writelines(["function set_signal_generator(){ \n", "siggen.enabled[0] = true \n", "siggen.enabled[1] = false \n"])
+        instr_config.writelines(["siggen.mode[0] = 1 \n", "siggen.waveform_type[0] = 0 \n"])
+        instr_config.write("siggen.waveform_frequency[0] = " + str(freq) + " \n")
+        instr_config.write("siggen.waveform_amplitude[0] = " + str(ampl) + " \n")
+        instr_config.write("siggen.waveform_offset[0] = " + str(offset) + " \n")
+        instr_config.write("siggen.waveform_phase[0] = 0 \n")
+        instr_config.write("siggen.running = true} \n")
+        instr_config.close()
+        
+        proc = subprocess.Popen(scopy_path + " --script " + m2k_config)
+        print("Launced Scopy and configured instruments.")
     
-# Create the x axis from 0 to numPeriods, divided into numSamples samples.
-x = np.linspace(0, numPeriods, numSamples)
-print(len(x))
+    return proc
 
-f1 = lambda x: amplitude1*np.sin(freq*2*np.pi*x)
-sampled_f1 = [f1(i) for i in x]
-print(len(sampled_f1))
-
-fig = plt.figure()
-fig.set_size_inches(12,6)
-plt.subplots_adjust(hspace=1)
-
-plt.subplot(311)
-plt.plot(x, sampled_f1)
-plt.title('Sine wave 50Hz')
-plt.xlabel('Time [sec]')
-plt.ylabel('Amplitude [arbitrary]')
-plt.xlim(0, 1)
-plt.ylim(-15, 15)
-plt.show()
-
-fft_output = np.fft.rfft(sampled_f1)
-magnitude = [np.sqrt(i.real**2+i.imag**2)/len(fft_output) for i in fft_output]
-frequencies = [(i*1.0/numSamples)*sRate for i in range(numSamples//2+1)]
-
-setup_graph(x_label='frequency [Hz]', y_label='amplitude [arbitrary]', 
-            title='Frequency Domain', fig_size=(12,6))
-plt.xlim(40,60)
-plt.plot(frequencies, magnitude, 'r')
-plt.show()
-
-len(sampled_f1)
-len(magnitude)
-len(frequencies)
-
+def func_off(instr, proc):
+    if instr == "m2k":
+        proc.kill()
+        subprocess.run('del instr_config.js', shell=True)
+        print("Successfully killed scopy!")
+        del proc
+    
+    return
